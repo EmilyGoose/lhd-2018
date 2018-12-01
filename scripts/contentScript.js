ce2436fa1ac551419cceae0d4c47a18981a6df2d
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2018-12-01 13:04:28 -0500

    contentScript.js searches for numbers

--- scripts/contentScript.js
+++ scripts/contentScript.js
@@ -4,10 +4,23 @@ $("p").each(function() {
     console.log($(this).text().split(' '));
     let words = $(this).text().split(' ');
     //Run through each word
+    for (let i = 0, len = words.length; i < len; ++i) {
+        //Check for numbers
+        if ($.isNumeric(words[i])) {
+            console.log("is NUMBER");
+            //Replaces numbers with a placeholder text
+            words[i] = "{{NUMBER PLACEHOLDER}}";
+        }
+    }
+
+    /*
     $(words).each(function () {
         //Check for numbers
         if ($.isNumeric(this)) {
-            console.log("NUMBER");
+            console.log("is NUMBER");
         }
     });
+    */
+    //Set p text content
+    $(this).text(words.join(" "));
 });
