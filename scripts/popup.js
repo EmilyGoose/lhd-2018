889a5b648a6b287a53cf383df62dbb48787e31be
Jonathan <jonathanxu25@gmail.com>
2018-12-01 17:02:59 -0500

    launch modal and able to get info

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,4 +1,9 @@
 $("#make-it-happen").click(function(){
-  console.log("cust");
   $("body").append("<p>cust</p>");
+
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, {type:"newMadlib", wordsToReplace:"noun, adjective, etc"}, function(response){
+      $("body").append("<p>" + response + "</p>");
+    });
+  });
 });
