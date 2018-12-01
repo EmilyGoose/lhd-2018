ca4c1a9649465477673570ff3dbf80594cb6a50c
Emily Goose <EmilyGoose@users.noreply.github.com>
2018-12-01 17:08:37 -0500

    Incomplete code

--- scripts/options.js
+++ scripts/options.js
@@ -1,31 +1,8 @@
-function save_options() {
-  var percentage = document.getElementById('percent').value;
-  var lines = [];
- $.each($('websites').val().split(/\n/), function(i, line){
-   if(line && line.length){
-      lines.push(line);
-   }
+
+$("#add-link").click(function() {
+    let linkInput = $("#link-input");
+    let newLink = linkInput.val();
+    linkInput.val("");
+
+    //Access the chrome storage
 });
-   function() {
-
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 0);
-  });
-}
-
-
-function restore_options() {
-
-  chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true
-  }, function(items) {
-  });
-
-}
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
