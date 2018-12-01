59c91889ea5520f4d834c0ebdc5e40b842c0899f
dora-su <dorasu02@gmail.com>
2018-12-01 17:33:00 -0500

    not working stff :(

--- scripts/options.js
+++ scripts/options.js
@@ -1,8 +1,21 @@
-
+let links = [];
 $("#add-link").click(function() {
     let linkInput = $("#link-input");
     let newLink = linkInput.val();
-    linkInput.val("");
 
+    linkInput.val("");
+    links.add(newLink);
     //Access the chrome storage
+
+});
+
+chrome.storage.onChanged.addListener(function(changes,){
+  for(var i = 0; i<=links.length; i++){
+    $("link-list").append{
+        "
+        <button class='remove-link' type='button' onclick='removeItemFromLinks("+id+")'>Remove</button>"
+
+      }
+  }();
+
 });
