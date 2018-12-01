7260f0a2c16c90a44c30a708e3d024c1177ca77a
AltheFlyer <30613228+AltheFlyer@users.noreply.github.com>
2018-12-01 14:03:33 -0500

    Pseudocode + random chance selector

--- scripts/contentScript.js
+++ scripts/contentScript.js
@@ -1,18 +1,34 @@
-
+let madlibRequest = [];
+let replacements = [];
+let newWord = "HELP";
 
 $("p").each(function() {
     console.log($(this).text().split(' '));
     let words = $(this).text().split(' ');
     //Run through each word
     for (let i = 0, len = words.length; i < len; ++i) {
+        //Random chance of checking:
+        if (Math.random() < 0.01) {
+            words[i] = "{{RANDOM PLACEHOLDER FOR:" + words[i] + "}}";
+            replacements.push(i);
+        }
         //Check for numbers
         if ($.isNumeric(words[i])) {
             console.log("is NUMBER");
             //Replaces numbers with a placeholder text
-            words[i] = "{{NUMBER PLACEHOLDER}}";
+            words[i] = "{{NUMBER PLACEHOLDER FOR:" + words[i] + "}}";
         }
+        /*
+        if (wordIsValid) {
+            if (Math.random() < chance) {
+                madlibRequest.push(word.type());
+            }
+        }
+         */
     }
-
+    for (let i = 0, len = replacements.length; i < len; ++i) {
+        words[replacements[i]] = newWord;
+    }
     /*
     $(words).each(function () {
         //Check for numbers
@@ -24,3 +40,10 @@ $("p").each(function() {
     //Set p text content
     $(this).text(words.join(" "));
 });
+
+/*Pseudocode to replace words in doc:
+for (word in wordReplacements) {
+    indexOfReplacement = words.indexOf("{{RANDOM_PLACEHOLDER")
+    words[indexOfReplacement] = word;
+}
+ */
