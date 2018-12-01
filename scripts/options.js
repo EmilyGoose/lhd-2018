f3b18456d6e20e431da20ba0a1113ba2e59d15d8
Emily Goose <EmilyGoose@users.noreply.github.com>
2018-12-01 18:05:41 -0500

    websites

--- scripts/options.js
+++ scripts/options.js
@@ -1,21 +1,30 @@
-let links = [];
 $("#add-link").click(function() {
     let linkInput = $("#link-input");
     let newLink = linkInput.val();
 
     linkInput.val("");
-    links.add(newLink);
-    //Access the chrome storage
+
+    chrome.storage.local.get({links: []}, function(result) {
+        console.log("Result: ");
+        console.log(result.valueOf());
+        let linkList = result.links;
+        console.log(linkList);
+        linkList.push(newLink);
+        chrome.storage.local.set({links: linkList}, function() {
+            console.log('Value is set to ' + linkList);
+        });
+    });
+
 
 });
 
-chrome.storage.onChanged.addListener(function(changes,){
-  for(var i = 0; i<=links.length; i++){
-    $("link-list").append{
-        "
-        <button class='remove-link' type='button' onclick='removeItemFromLinks("+id+")'>Remove</button>"
-
-      }
-  }();
-
+chrome.storage.onChanged.addListener(function(changes, namespace){
+    if ("links" in changes) {
+        let newLinkList = changes["links"].newValue;
+        console.log(newLinkList);
+        console.log(newLinkList.length);
+        for (let i = 0; i < newLinkList.length; i++) {
+            $("#link-list").append("<p>" + newLinkList[i] + "</p>");
+        }
+    }
 });
