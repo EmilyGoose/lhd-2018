be52a9ff27db520e5e76b4cbfeb7e9c9645cd938
Jonathan <jonathanxu25@gmail.com>
2018-12-01 14:39:29 -0500

    modal popup works

--- scripts/contentScript.js
+++ scripts/contentScript.js
@@ -1,3 +1,30 @@
+$(document).ready(function(){
+  $("body").append(`
+    <button id="myBtn">Open Modal</button>
+
+    <div id="myModal" class="modal">
+
+      <div class="modal-content">
+        <span class="close">&times;</span>
+        <p>Some text in the Modal..</p>
+      </div>
+
+    </div>
+  `);
+
+  // Get the modal
+  var modal = $("#myModal");
+  var btn = $("#myBtn");
+  var span = $(".close");
+
+  btn.click(function() {
+      modal.css('display', 'inline-block');
+  });
+  span.click(function() {
+      modal.css('display', 'none');
+  });
+})
+
 
 
 $("p").each(function() {
